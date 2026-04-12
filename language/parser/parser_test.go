296df8abd6a51bce2407e129253e6ac057aa06db
Eloy3 <Eloy3@github.com>
2026-04-12 19:34:07 +0200

    feat: add parser tests

--- language/parser/parser_test.go
+++ language/parser/parser_test.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"deadlock/language/ast"
 	"deadlock/language/token"
+	"fmt"
 
 	"testing"
 )
@@ -39,4 +41,66 @@ func TestIdentifierExpression(t *testing.T) {
 	if len(statements) != 1 {
 		t.Fatalf("program has not enough statements. got=%d", len(statements))
 	}
+}
+
+func TestParsingPrefixExpressions(t *testing.T) {
+	prefixTests := []struct {
+		input        string
+		operator     string
+		integerValue int64
+	}{
+		{"!5;", "!", 5},
+		{"-15;", "-", 15},
+	}
+
+	for _, tt := range prefixTests {
+		tokens, _ := token.TokenizeProgram(tt.input)
+		parser := NewParser(tokens)
+		program := parser.ParseProgram()
+		checkParserErrors(t, parser)
+
+		if len(program) != 1 {
+			t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+				1, len(program))
+		}
+
+		stmt, ok := program[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+				program[0])
+		}
+
+		exp, ok := stmt.Expression.(*ast.PrefixExpression)
+		if !ok {
+			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+		}
+
+		if exp.Operator != tt.operator {
+			t.Fatalf("exp.Operator is not '%s'. got=%s",
+				tt.operator, exp.Operator)
+		}
+
+		if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
+			return
+		}
+	}
+
+}
+
+func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
+	integ, ok := il.(*ast.IntegerLiteral)
+	if !ok {
+		t.Errorf("il not *ast.IntegerLiteral. got=%T", il)
+		return false
+	}
+	if integ.Value != value {
+		t.Errorf("integ.Value not %d. got=%d", value, integ.Value)
+		return false
+	}
+	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
+		t.Errorf("integ.TokenLiteral not %d. got=%s", value,
+			integ.TokenLiteral())
+		return false
+	}
+	return true
 }
